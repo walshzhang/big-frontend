07f3db8247bc8aed3942e65ff44d5783ed3b97fa
walsh <walsh.zhang@hotmail.com>
2019-06-28 10:58:22 +0800

    add remove to js code

--- static-web/index.js
+++ static-web/index.js
@@ -4,3 +4,17 @@ function toggle(checkbox) {
   var tr = td.parentNode;
   tr.className = className;
 }
+
+function remove(button) {
+  var td = button.parentNode;
+  var tr = td.parentNode;
+
+  var tbody = document.getElementsByTagName('tbody')[0];
+
+  for (var i = 0; i < tbody.rows.length; i++) {
+    if (tr === tbody.rows[i]) {
+      tbody.deleteRow(i);
+      break;
+    }
+  }
+}
